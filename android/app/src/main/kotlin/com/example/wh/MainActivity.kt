25c24800b1b7a1b74306d86e0cc9c8f9bd358d87
wanghao <w_hao1987@163.com>
2023-04-03 00:26:28 +0800

    创建BasicMessageChannel 消息通道用于Android  与flutter相互通信

--- android/app/src/main/kotlin/com/example/wh/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wh/MainActivity.kt
@@ -1,6 +1,30 @@
 package com.example.wh
 
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.BasicMessageChannel
+import io.flutter.plugin.common.StringCodec
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    // Activity 初始化Flutter引擎 回调
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+// 初始化 BasicMessageChannel 并传入名称，名称要和Dart端一致
+        var channel = BasicMessageChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "messageChannel",
+            StringCodec.INSTANCE
+        )
+//  通过BasicMessageChannel实例注册接受回调，并且返回ack
+        channel.setMessageHandler { message, reply ->
+            Log.d("flutter","android receive message : ${message}")
+            reply.reply("Android 端将信息传递给Flutter---------------")
+
+
+        }
+        // 通过 BasicMessageChannel实例发送消息给Dart
+        channel.send("hello from android-------------------------->>>>>>")
+
+    }
 }
