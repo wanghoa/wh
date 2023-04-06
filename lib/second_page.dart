4c810a5920d3bf2034984f46547c223e94224c74
wanghao <w_hao1987@163.com>
2023-04-07 00:54:52 +0800

    通过构造 传递参数

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:wh/main.dart';
+
 
 class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
+  final String params;
+  const SecondPage({super.key,this.params=''});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
         child: const Text('第二个页面'),
         onTap: () {
+          print('SecondPage-Prams: $params');
+
+
           var result = 'ACK from secondPage 返回值';
           //todo
+          router.popRoute(params: result);
         });
     throw UnimplementedError();
   }
