3eca3e4b227e0c143114d6ab34813409e19de319
wanghao <w_hao1987@163.com>
2023-04-07 01:24:39 +0800

    创建播放页面

---
+++ lib/player_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class PlayerPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _PlayerPageState();
+  }
+}
+
+class _PlayerPageState extends State<PlayerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return
+  }
+}
