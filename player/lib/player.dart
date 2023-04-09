8a02a73b6cb4f8614f2e158a7d2a660ee039e2a7
wanghao <w_hao1987@163.com>
2023-04-10 02:09:54 +0800

    编写 Player插件内代码

--- player/lib/player.dart
+++ player/lib/player.dart
@@ -1,8 +1,21 @@
+import 'package:fijkplayer/fijkplayer.dart';
 
 import 'player_platform_interface.dart';
 
-class Player {
+class Player extends FijkPlayer {
   Future<String?> getPlatformVersion() {
     return PlayerPlatform.instance.getPlatformVersion();
   }
+  static const  asset_url_suffix='null';
+  void setCommonDataSource(String url,
+      {SourceType type = SourceType.net,
+      bool autoPlay = false,
+      bool showCover = false}) {
+    if (type ==  SourceType.asset && !url.startsWith(asset_url_suffix)) {
+
+    }
+    setDataSource(url,autoPlay: autoPlay,showCover: showCover);
+  }
 }
+
+enum SourceType { net, asset }
---
+++ player/lib/video_view.dart
@@ -0,0 +1,30 @@
+import 'package:fijkplayer/fijkplayer.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:player/player.dart';
+
+class VideoView extends StatefulWidget {
+  final Player player;
+
+  const VideoView(this.player)
+
+  @override
+  State<StatefulWidget> createState() {
+    return _VideoViewState();
+  }
+}
+
+class _VideoViewState extends State<VideoView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Stack(
+      children: [FijkView(player: widget.player),
+        // Image.asset('asset/images/play.png')
+        Align(child: Image.asset('name', width: 60, height: 60),
+          alignment: Alignment.center,)
+
+      ],
+    ));
+  }
+
+}
