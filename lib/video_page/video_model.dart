47412b46b37d9d24211b37471e4226c47c30be7b
wanghao <w_hao1987@163.com>
2023-04-11 01:21:27 +0800

    创建对象转json 方法

--- lib/video_page/video_model.dart
+++ lib/video_page/video_model.dart
@@ -10,6 +10,9 @@ class VideoModel {
   VideoModel(this.title, this.url, this.playCount); // win+n (MAC Command+N)
 
   // 相当于Java实现工厂设计模式
-  factory VideoModel.fromJson(Map<String, dynamic> json) =>
-      VideoModel(json['title'], json['url'], json['playCount']);// 这里的title,url,playCount 要与服务端一致
+  factory VideoModel.fromJson(Map<String, dynamic> json) => _$VideoModelFromJson(json);
+  //     // VideoModel(json['title'], json['url'], json['playCount']);// 这里的title,url,playCount 要与服务端一致
+  //
+  Map<String, dynamic> toJson(VideoModel instance) => _$VideoModelToJson( instance);
+
 }
