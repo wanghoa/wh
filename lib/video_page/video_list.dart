e6cfac2ce532173b40c0839bcfaf466b8f3141b3
wanghao <w_hao1987@163.com>
2023-04-11 03:32:32 +0800

    格式化代码

--- lib/video_page/video_list.dart
+++ lib/video_page/video_list.dart
@@ -12,15 +12,16 @@ class VideoList extends StatefulWidget {
 }
 
 class _VideoListState extends State<VideoList> {
-
   late VideoController _controller;
+
   // VideoController _controller = VideoController();
 
   @override
   void initState() {
     super.initState();
-    _controller =VideoController(); //这种初始化方式，如果在_VideoListState被复用场景 一个_VideoListState对应一个 VideoController
-    _controller.init().then((value) => setState((){}));
+    _controller =
+        VideoController(); //这种初始化方式，如果在_VideoListState被复用场景 一个_VideoListState对应一个 VideoController
+    _controller.init().then((value) => setState(() {}));
   }
 
   @override
@@ -35,19 +36,20 @@ class _VideoListState extends State<VideoList> {
               // var url =
               //     'https://sample-videos.com/video123/flv/240/big_buck_bunny_240p_10mb.flv';
               return GestureDetector(
-                  child: _controller.model== null
-                ?Container():
-                  AbsorbPointer(
-                    absorbing: true,
-                    child: VideoView(Player()
-                      ..setCommonDataSource(
-                        _controller.model?.url??'',
-                        type: SourceType.asset,
-                        autoPlay: true,
-                      )),
-                  ),
+                  child: _controller.model == null
+                      ? Container()
+                      : AbsorbPointer(
+                          absorbing: true,
+                          child: VideoView(Player()
+                            ..setCommonDataSource(
+                              _controller.model?.url ?? '',
+                              type: SourceType.asset,
+                              autoPlay: true,
+                            )),
+                        ),
                   onTap: () async => await router.push(
-                      name: MCRouter.playerPage, argument: _controller.model?.url??''));
+                      name: MCRouter.playerPage,
+                      argument: _controller.model?.url ?? ''));
             }));
   }
 }
