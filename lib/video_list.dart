c16027a85b8ed02450d091dbe2d986802773903b
wanghao <w_hao1987@163.com>
2023-04-10 22:59:49 +0800

    处理Scaffold 报错

--- lib/video_list.dart
+++ lib/video_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:player/player.dart';
 import 'package:player/video_view.dart';
 import 'package:wh/main.dart';
