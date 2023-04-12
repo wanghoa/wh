6c7dade9ab04d35731ef21336290cf2de4057008
wanghao <w_hao1987@163.com>
2023-04-12 23:01:53 +0800

    创建显示 红心 的widget

---
+++ lib/favorite_gesture.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FavoriteGesture extends StatefulWidget {
+
+
+  @override
+  State<StatefulWidget> createState() => _FavoriteGestureState();
+}
+
+class _FavoriteGestureState extends State<FavoriteGesture> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(// 堆叠（FrameLayout）
+        children: [
+      Container(width: double.infinity, color: Colors.black),
+      const Icon(Icons.favorite, size: 100, color: Colors.redAccent)
+    ]);
+  }
+}
