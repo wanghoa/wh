93addecdce3fccfa3a563a58598e752e8e840c41
wanghao <w_hao1987@163.com>
2023-04-13 02:37:55 +0800

    格式化

--- lib/favorite_animation_icon.dart
+++ lib/favorite_animation_icon.dart
@@ -4,13 +4,15 @@ import 'package:flutter/material.dart';
 class FavoriteAnimationIcon extends StatefulWidget {
   final double size;
 
-  const FavoriteAnimationIcon({required Key key ,required this.size}):super(key: key); //  此处是否添加 const 从代码表面看没什么区别；
+  const FavoriteAnimationIcon({required Key key, required this.size})
+      : super(key: key); //  此处是否添加 const 从代码表面看没什么区别；
 
   @override
   State<FavoriteAnimationIcon> createState() => _FavoriteAnimationIconState();
 }
 
-class _FavoriteAnimationIconState extends State<FavoriteAnimationIcon> with TickerProviderStateMixin{
+class _FavoriteAnimationIconState extends State<FavoriteAnimationIcon>
+    with TickerProviderStateMixin {
   late AnimationController _animationController;
   static const int _duration = 600; // 动画时长
   _FavoriteAnimationIconState();
@@ -18,7 +20,9 @@ class _FavoriteAnimationIconState extends State<FavoriteAnimationIcon> with Tick
   @override
   void initState() {
     super.initState();
-    _animationController =  AnimationController(duration: const Duration(milliseconds: _duration), vsync: this);// this ->TickerProviderStateMixin
+    _animationController = AnimationController(
+        duration: const Duration(milliseconds: _duration),
+        vsync: this); // this ->TickerProviderStateMixin
   }
 
   @override
