700916fbebf8d2becd70ce1a54ac22f792f241e9
wanghao <w_hao1987@163.com>
2023-04-12 16:06:59 +0800

    添加数据

--- lib/video_page/server_data.dart
+++ lib/video_page/server_data.dart
@@ -1,11 +1,55 @@
 class ServerData {
-
-  static String fetchDataFromServer() {
-    //模拟网络获取数据，在实际项目中是从服务端接口获取数据
-    return """
-     "title":"实例视频"，
-     "url","https://sample-videos.com/video123/flv/240/big_buck_bunny_240p_10mb.flv",
-     "playCount":66} 
-      """;
-  }
+  // 本地mock数据，实际上是模拟网络数据
+  static String fetchDataFromServer() => """[
+    {
+        "title": "示例视频",
+        "url": "http://vfx.mtime.cn/Video/2019/03/19/mp4/190319212559089721.mp4",
+        "playCount": 88
+    },
+    {
+        "title": "示例视频",
+        "url": "https://prod-streaming-video-msn-com.akamaized.net/a8c412fa-f696-4ff2-9c76-e8ed9cdffe0f/604a87fc-e7bc-463e-8d56-cde7e661d690.mp4",
+        "playCount": 88
+    },
+    {
+        "title": "示例视频",
+        "url": "https://prod-streaming-video-msn-com.akamaized.net/0b927d99-e38a-4f51-8d1a-598fd4d6ee97/3493c85c-f35a-488f-9a8f-633e747fb141.mp4",
+        "playCount": 88
+    },
+    {
+        "title": "示例视频",
+        "url": "https://prod-streaming-video-msn-com.akamaized.net/fe13f13c-c2cc-4998-b525-038b23bfa9b5/1a9d30ca-54be-411e-8b09-d72ef4488e05.mp4",
+        "playCount": 88
+    },
+    {
+        "title": "示例视频",
+        "url": "https://vfx.mtime.cn/Video/2019/08/24/mp4/190824113155647173.mp4",
+        "playCount": 88
+    },
+    {
+        "title": "示例视频",
+        "url": "https://prod-streaming-video-msn-com.akamaized.net/e908e91f-370f-49ad-b4ce-775b7e7a05b4/a6287f74-46f0-42f9-b5d9-997f00585696.mp4",
+        "playCount": 88
+    },
+    {
+        "title": "示例视频",
+        "url": "https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218114723HDu3hhxqIT.mp4",
+        "playCount": 88
+    },
+    {
+        "title": "示例视频",
+        "url": "https://klxxcdn.oss-cn-hangzhou.aliyuncs.com/histudy/hrm/media/bg2.mp4",
+        "playCount": 88
+    },
+    {
+        "title": "示例视频",
+        "url": "https://sample-videos.com/video123/flv/240/big_buck_bunny_240p_10mb.flv",
+        "playCount": 88
+    },
+    {
+        "title": "示例视频",
+        "url": "https://sample-videos.com/video123/flv/240/big_buck_bunny_240p_10mb.flv",
+        "playCount": 88
+    }
+]""";
 }
