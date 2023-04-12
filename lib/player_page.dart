82cd6ae2265e3cb412ab25f2a10fd38fd8011a4c
wanghao <w_hao1987@163.com>
2023-04-13 01:50:47 +0800

    播放页添加双击事件（FavoriteGesture）展示 点赞效果

--- lib/player_page.dart
+++ lib/player_page.dart
@@ -5,10 +5,17 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:player/player.dart';
 import 'package:player/video_view.dart';
+import 'package:wh/favorite_gesture.dart';
 
+/*
+ 播放页面存在三层View；双击 是点赞；长按： 下载；单击是暂停播放
+ 顶层：双击事件 然后是长按,最里面是点击
+ */
 class PlayerPage extends StatefulWidget {
   final String videoUrl;
-  const PlayerPage({this.videoUrl=''});
+
+  const PlayerPage({this.videoUrl = ''});
+
   @override
   State<StatefulWidget> createState() {
     return _PlayerPageState();
@@ -33,9 +40,10 @@ class _PlayerPageState extends State<PlayerPage> {
   @override
   Widget build(BuildContext context) {
     var player = Player();
-    player.setCommonDataSource(widget.videoUrl, type:SourceType.net,autoPlay: true);
-    // 长按视频弹窗
-    return GestureDetector(
+    player.setCommonDataSource(
+        widget.videoUrl, type: SourceType.net, autoPlay: true);
+    // FavoriteGesture 内部实现双击事件
+    return FavoriteGesture(child: GestureDetector( // 长按视频弹窗
         onLongPress: () {
           showDialog(
               context: context,
@@ -57,7 +65,7 @@ class _PlayerPageState extends State<PlayerPage> {
                 );
               });
         },
-        child: VideoView(player));
+        child: VideoView(player));)
   }
 
   /**
@@ -71,13 +79,13 @@ class _PlayerPageState extends State<PlayerPage> {
     // var path = getAppPath();// 内部使用MethodChannel 调用到native层。分别在Android与iOS上实现MethodChannel。返回用户可以保存的路径
     // 这里我们使用第三方库也是使用MethodChannel
     var dir =
-        await getExternalStorageDirectory(); // path_provider库  获取sdcart视频存储路径；Android与iOS分别获取不同路径
+    await getExternalStorageDirectory(); // path_provider库  获取sdcart视频存储路径；Android与iOS分别获取不同路径
     String savePath = "${dir?.path}/$name";
     var result = await Dio().download(urlPath, savePath,
         onReceiveProgress: (count, total) {
-      var progress = '${(count / total * 100).toInt()}%';
-      print('打印下载进度：$progress');
-    });
+          var progress = '${(count / total * 100).toInt()}%';
+          print('打印下载进度：$progress');
+        });
     print('result:$result');
   }
 
