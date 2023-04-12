e007dfaa67bdceeae6c8b1298763d5117383e38d
wanghao <w_hao1987@163.com>
2023-04-12 23:12:14 +0800

    默认不展示红心icon，双击刷新并展示红心

--- lib/favorite_gesture.dart
+++ lib/favorite_gesture.dart
@@ -4,13 +4,15 @@ import 'package:flutter/material.dart';
 class FavoriteGesture extends StatefulWidget {
   final Widget child;
 
-  const FavoriteGesture({required this.child, Key? key}) :super(key: key);
+  const FavoriteGesture({required this.child, Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _FavoriteGestureState();
 }
 
 class _FavoriteGestureState extends State<FavoriteGesture> {
+  bool isFavorite = false;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -19,12 +21,20 @@ class _FavoriteGestureState extends State<FavoriteGesture> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(child: Stack( // 堆叠（FrameLayout）
-        children: [
+    return GestureDetector(
+        child: Stack(// 堆叠（FrameLayout）
+            children: [
           Container(width: double.infinity, color: Colors.black),
-          const Icon(Icons.favorite, size: 100, color: Colors.redAccent)
-        ]),onDoubleTap: (){
-         // 双击事件
-    });
+          if (isFavorite)
+            //默认 Icon 不显示
+            const Icon(Icons.favorite, size: 100, color: Colors.redAccent)
+        ]),
+        onDoubleTap: () {
+          // 双击事件  双击刷新页面
+          setState(() {
+
+          });
+
+        });
   }
 }
