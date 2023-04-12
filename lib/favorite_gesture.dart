93eabf596b0cf57d5f228953d207960725cc55c9
wanghao <w_hao1987@163.com>
2023-04-13 00:05:37 +0800

    定义变量，常量

--- lib/favorite_gesture.dart
+++ lib/favorite_gesture.dart
@@ -2,9 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteGesture extends StatefulWidget {
+  static const double defaultSize = 100; //定义常量
   final Widget child;
+  final double? size;
 
-  const FavoriteGesture({required this.child, Key? key}) : super(key: key);
+  const FavoriteGesture(
+      {required this.child, this.size = defaultSize, Key? key})
+      : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _FavoriteGestureState();
@@ -12,25 +16,30 @@ class FavoriteGesture extends StatefulWidget {
 
 class _FavoriteGestureState extends State<FavoriteGesture> {
   bool isFavorite = false;
-  Offset temp =  Offset.zero;//Offset 存储坐标的
+  Offset temp = Offset.zero; //Offset 存储坐标的
 
   @override
   void initState() {
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        child: Stack(// 堆叠（FrameLayout）
+        child: Stack( // 堆叠（FrameLayout）
             children: [
-          Container(width: double.infinity, color: Colors.black),// 这里Container 代替VideoView;
-          if (isFavorite)
-            //默认 Icon 不显示
-            const Icon(Icons.favorite, size: 100, color: Colors.redAccent)
-        ]),
-        onTapDown: (details){
+              Container(width: double.infinity, color: Colors.black),
+              // 这里Container 代替VideoView;
+              if (isFavorite)
+              // 作具体位置的摆放  //默认 Icon 不显示
+                Positioned(child: const Icon(
+                    Icons.favorite, size: widget.size, color: Colors.redAccent),
+                  top: temp.dy - widget.size / 2,
+                  left:temp.dx - widget.size/2,)
+
+
+            ]),
+        onTapDown: (details) {
           temp = details.globalPosition;
         },
         onDoubleTap: () {
