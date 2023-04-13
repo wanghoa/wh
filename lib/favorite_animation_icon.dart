8f8b0e5b50e6dbf47b0fbd51c0f4e29f83a7505b
wanghao <w_hao1987@163.com>
2023-04-13 11:53:53 +0800

    完善动画细节

--- lib/favorite_animation_icon.dart
+++ lib/favorite_animation_icon.dart
@@ -2,9 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteAnimationIcon extends StatefulWidget {
+  final Offset position;///保存每个坐标
   final double size;
+  final Function? onAnimationComplete;
 
-  const FavoriteAnimationIcon({required Key key, required this.size})
+  const FavoriteAnimationIcon(
+      {required Key key,
+      required this.size,
+      required this.position,
+      this.onAnimationComplete})
       : super(key: key); //  此处是否添加 const 从代码表面看没什么区别；
 
   @override
@@ -15,6 +21,12 @@ class _FavoriteAnimationIconState extends State<FavoriteAnimationIcon>
     with TickerProviderStateMixin {
   late AnimationController _animationController;
   static const int _duration = 600; // 动画时长
+  // 展示的进度值
+  static const double appearValue = 0.1;
+
+  // 消失的进度值
+  static const double dismissValue = 0.8;
+
   _FavoriteAnimationIconState();
 
   @override
@@ -23,10 +35,50 @@ class _FavoriteAnimationIconState extends State<FavoriteAnimationIcon>
     _animationController = AnimationController(
         duration: const Duration(milliseconds: _duration),
         vsync: this); // this ->TickerProviderStateMixin
+    // 监听进度
+    _animationController.addListener(() {
+      setState(() {}); // 重绘
+    });
+
+    // 开始动画逻辑
+    startAnimation();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Icon(Icons.favorite, size: widget.size, color: Colors.redAccent);
+    var content =
+        Icon(Icons.favorite, size: widget.size, color: Colors.redAccent);
+    return Positioned(
+        top: widget.position.dy - widget.size! / 2,
+        left: widget.position.dx - widget.size! / 2,
+        child: Opacity(opacity: opacity, child: content)); // 透明度动画
+  }
+
+  double get opacity {
+    if (value < appearValue) {
+      // 处于渐进阶段。播放透明度动画
+      return 1 / appearValue * value; //
+    }
+    if (value < dismissValue) {
+      return 1;
+    }
+    return (value - dismissValue) / (1 - dismissValue) + 1;
+  }
+
+  double get value {
+    return _animationController.value;
+  }
+
+  void startAnimation() async {
+    await _animationController.forward();
+
+    /// await 是同步方法 动画播放完成才会向下执行代码
+    widget.onAnimationComplete?.call();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose(); //回收资源
   }
 }
